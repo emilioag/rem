b887083551e2bd2456b266d450b209f67447e8c5
emilioag <agonzalez.emilio@gmail.com>
2017-08-14 12:29:26 +0200

    New method walk

--- script/lib.py
+++ script/lib.py
@@ -1,5 +1,6 @@
 import io
 import re
+import os
 
 
 def replace_file(replace, by, file_name):
@@ -21,3 +22,13 @@ def replace_lines(replace, by, lines):
 def replace_line(replace, by, line):
     new = re.sub(replace, by, line)
     return new
+
+
+def walk(dir_name: str, ignore: list, extension: str = None) -> iter:
+    for path, dirs, files in os.walk(dir_name, topdown=True):
+        dirs[:] = [d for d in dirs if d not in ignore]
+        if extension:
+            files[:] = [
+                f for f in files if f.endswith('.{}'.format(extension.lstrip('.')))
+            ]
+        yield (path, dirs, files)
